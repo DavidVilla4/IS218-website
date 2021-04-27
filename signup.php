bcf48b0900aad02162766a2755c7866e59ce4c81
Nick <nhoffmann117@gmail.com>
2021-04-26 21:44:32 -0400

    Added basic bootstrap styling

--- signup.php
+++ signup.php
@@ -84,6 +84,7 @@ if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["passwor
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 <form action="signup.php" autocomplete="off" method="post">
