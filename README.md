8cdca4df36dedb492a1bb3e939783fc40be82221
Nick <nhoffmann117@gmail.com>
2021-05-05 12:17:05 -0400

    Added instructions for setting up database

---
+++ README.md
@@ -0,0 +1,8 @@
+# IS218 Final Project Site
+
+## Instructions
+1. Ensure that you do not already have tables named "accounts" or "tasks" in your database
+2. Navigate to "config" directory of this project
+3. Import "is218.sql" into your database
+4. Open "dbcredentials.php"
+5. Edit values of the variables in "dbcredentials.php" to match the credentials of your database and save
--- config/dbcredentials.php
+++ config/dbcredentials.php
@@ -1,5 +1,5 @@
 <?php
-$dbhost = "localhost";
-$dbname = "is218_project";
-$dbuname = "root";
-$dbpass = "";
+$dbhost = "localhost";      // Server host, i.e. "localhost" or "sql.njit.edu"
+$dbname = "is218_project";  // Name of the database i.e. "is218_project" or whatever you named it
+$dbuname = "root";          // Username to log in to your database with, such as your UCID if using NJIT's database
+$dbpass = "";               // Your password to log into your database
